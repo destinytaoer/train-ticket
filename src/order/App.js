01cb842001b99d00c722c9866955526afc58b9c2
destinytaoer <1848765519@qq.com>
2020-02-13 15:22:16 +0800

    fix[build]修复上一个提交导致无法 build 的 BUG
    1. 安装 redux 等模块
    2. 修改 webpack 配置中的 manifest 插件配置
    3. 修改相关文件的引用错误

---
+++ src/order/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import './App.css';
+
+function App(props) {
+  return <div>order</div>
+}
+
+export default connect(
+  state => { return {} },
+  dispatch => {return {}}
+)(App);
--- src/query/index.js
+++ src/query/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
+import 'normalize.css/normalize.css'
 
 // 引入样式文件/store/根组件, 完成 index 的初始化
 import store from "./store";
-import 'normalize.css/normalize.css'
 import './index.css'
-import App from 'App.jsx'
+import App from './App'
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>,
