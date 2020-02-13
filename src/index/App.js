b0a6f5c9bab4a0a3b3d103b1e4e6ebaf91af120d
destinytaoer <1848765519@qq.com>
2020-02-13 16:51:05 +0800

    chore[index] 完成 index 页面组件设计
    1. 公共 Header 组件
    2. 始发站到终点站 Journey 组件
    3. 日期选择 DepartDate 组件
    4. 高铁/动车筛选 HighSpeed 组件
    5. 提交按钮 Submit 组件

--- src/index/App.js
+++ src/index/App.js
@@ -2,8 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 
+import Header from '../common/Header';
+import Journey from './components/Journey';
+import DepartDate from './components/DepartDate';
+import HighSpeed from './components/HighSpeed';
+import Submit from './components/Submit';
+
 function App(props) {
-  return <div>index</div>
+  return (
+    <div>
+      <Header />
+      <Journey />
+      <DepartDate />
+      <HighSpeed />
+      <Submit />
+    </div>
+  )
 }
 
 export default connect(
