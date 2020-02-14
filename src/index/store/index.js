f2ca3fa50973f53c7cda3f78d498c3c1bdf9443d
destinytaoer <1848765519@qq.com>
2020-02-14 14:15:53 +0800

    fix(index)  修复配置 redux-devtools 错误的 bug

--- src/index/store/index.js
+++ src/index/store/index.js
@@ -3,7 +3,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import reducer from './reducers';
 import thunk from 'redux-thunk';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENDTION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default createStore(
   reducer,
   composeEnhancers(applyMiddleware(thunk))
