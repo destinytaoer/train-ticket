f35587de19a3b92244c6afcd1dd7f033d397b6aa
destinytaoer <1848765519@qq.com>
2020-02-14 14:44:03 +0800

    feat(index) 完成城市切换组件 Journey

--- src/index/App.js
+++ src/index/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useCallback, useMemo} from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
+import * as actions from './store/actions';
 import './App.css';
 
 import Header from '../common/Header';
@@ -9,20 +11,42 @@ import HighSpeed from './components/HighSpeed';
 import Submit from './components/Submit';
 
 function App(props) {
+  const { 
+    from,
+    to,
+    dispatch
+  } = props;
+
+  const cbs = useMemo(() => {
+    // 将 bindActionCreators 放到这里, 防止每次组件重新渲染都重新执行, 生成新的函数导致子组件重新渲染
+    return bindActionCreators(actions, dispatch);
+  }, [dispatch]); // dispatch 不会改变, 这里使用空数组也可以
+  
+  const handleBack = useCallback(() => {
+    window.history.back();
+  },[])
+
   return (
     <div>
       <div className="header-wrapper">
-        <Header title="火车票" />
+        <Header title="火车票" handleBack={handleBack} />
       </div>
-      <Journey />
-      <DepartDate />
-      <HighSpeed />
-      <Submit />
+      <form className="form">
+        <Journey
+          from={from}
+          to={to}
+          exchangeFromTo={cbs.exchangeFromTo}
+          showCitySelector={cbs.showCitySelector}
+        />
+        <DepartDate />
+        <HighSpeed />
+        <Submit />
+      </form>
     </div>
   )
 }
 
 export default connect(
-  state => { return {} },
-  dispatch => {return {}}
+  state => { return {...state} },
+  dispatch => {return {dispatch}}
 )(App);
--- src/index/components/Journey.js
+++ src/index/components/Journey.js
@@ -1,8 +1,53 @@
 import React from 'react';
-import './Journey.css'
+import PropTypes from 'prop-types';
+import switchImg from './imgs/switch.svg'
+import './Journey.css';
 
-export default function Journey(props) {
+function Journey(props) {
+  const { 
+    from,
+    to,
+    exchangeFromTo,
+    showCitySelector
+   } = props;
   return (
-    <div></div>
+    <div className="journey">
+      <div
+        className="journey-station"
+        onClick={() => showCitySelector(true)}
+      >
+        <input
+          type="text"
+          readOnly
+          name="from"
+          className="journey-input journey-from"
+          value={from}
+        />
+      </div>
+      <div className="journey-switch" onClick={exchangeFromTo}>
+        <img src={switchImg} width="70" height="40" alt="switch"/>
+      </div>
+      <div
+        className="journey-station"
+        onClick={() => showCitySelector(false)}
+      >
+        <input
+          type="text"
+          readOnly
+          name="to"
+          className="journey-input journey-to"
+          value={to}
+        />
+      </div>
+    </div>
   )
 }
+
+Journey.propTypes = {
+  from: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+  exchangeFromTo: PropTypes.func.isRequired,
+  showCitySelector: PropTypes.func.isRequired
+}
+
+export default Journey;
