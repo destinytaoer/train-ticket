d5769b03bce715390e679fe0b5d6fa735b2fca1c
destinytaoer <1848765519@qq.com>
2020-02-19 13:56:28 +0800

    feat(ticket) 完成 Header 部分

--- src/ticket/App.js
+++ src/ticket/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect, useMemo, useCallback } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actions from './store/actions';
+import URI from 'urijs';
+import dayjs from 'dayjs';
+import { h0 } from '../common/fp';
 
 import './App.css';
+import Header from '../common/Header';
 import Detail from './components/Detail';
 import Candidate from './components/Candidate';
 import Schedule from './components/Schedule';
@@ -24,7 +28,44 @@ function App(props) {
     dispatch
   } = props;
 
-  return <div>ticket</div>;
+  const cbs = useMemo(() => {
+    return bindActionCreators(actions, dispatch);
+  }, [dispatch]);
+
+  useEffect(() => {
+    const queries = URI.parseQuery(window.location.search);
+    const { aStation, dStation, trainNumber, date } = queries;
+
+    const {
+      setLeaveDate,
+      setLeaveStation,
+      setArriveStation,
+      setTrainNumber,
+      setSearchParsed
+    } = cbs;
+
+    // 需要注意的是, 解析出来的都是字符串
+    setLeaveDate(h0(dayjs(date).valueOf()));
+    setLeaveStation(dStation);
+    setArriveStation(aStation);
+    setTrainNumber(trainNumber);
+
+    setSearchParsed(true);
+  }, [cbs]);
+
+  const handleBack = useCallback(() => {
+    window.history.back();
+  }, []);
+
+  if (!isSearchParsed) return null;
+
+  return (
+    <div className='app'>
+      <div className='header-wrapper'>
+        <Header title={trainNumber} handleBack={handleBack} isBackVisible={true} />
+      </div>
+    </div>
+  );
 }
 
 export default connect(
