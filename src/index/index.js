ae24c63c7740ef9a9934647d77b52e7e038b5051
destinytaoer <1848765519@qq.com>
2020-02-20 17:22:36 +0800

    feat(all) 开启 PWA

--- src/index/index.js
+++ src/index/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-import 'normalize.css/normalize.css'
+import { Provider } from 'react-redux';
+import 'normalize.css/normalize.css';
+import * as serviceWorker from '../serviceWorker';
 
 // 引入样式文件/store/根组件, 完成 index 的初始化
-import store from "./store";
-import './index.css'
-import App from './App'
+import store from './store';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-)
+);
+
+if (process.env.NODE_ENV === 'production') {
+  serviceWorker.register();
+} else {
+  serviceWorker.unregister();
+}
