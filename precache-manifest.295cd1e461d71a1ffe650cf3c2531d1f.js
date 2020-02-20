a5e36c4f8be8f51d4b3c72a4248cc2d05c050cf1
destinytaoer <1848765519@qq.com>
2020-02-20 17:27:26 +0800

    Updates

---
+++ precache-manifest.295cd1e461d71a1ffe650cf3c2531d1f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af604d474b4b15d19218a85c11aa8380",
+    "url": "/train-ticket/index.html"
+  },
+  {
+    "revision": "adc098e0ed9d39a0682c99a8b78a448f",
+    "url": "/train-ticket/order.html"
+  },
+  {
+    "revision": "d378935994846b64e21461210ca11624",
+    "url": "/train-ticket/query.html"
+  },
+  {
+    "revision": "06704d143edb6cf96073",
+    "url": "/train-ticket/static/css/0.764ccc25.chunk.css"
+  },
+  {
+    "revision": "cef3fd16e8d70c00ad8e",
+    "url": "/train-ticket/static/css/10.ab901911.chunk.css"
+  },
+  {
+    "revision": "6f0ad54daca6af4bac12",
+    "url": "/train-ticket/static/css/index.ab6ac582.chunk.css"
+  },
+  {
+    "revision": "75104e727b314eb678a2",
+    "url": "/train-ticket/static/css/order.f64b955f.chunk.css"
+  },
+  {
+    "revision": "c045949d961d82d34773",
+    "url": "/train-ticket/static/css/query.b6ff1da1.chunk.css"
+  },
+  {
+    "revision": "9e67924f8bde494dc79d",
+    "url": "/train-ticket/static/css/ticket.e8c005b1.chunk.css"
+  },
+  {
+    "revision": "06704d143edb6cf96073",
+    "url": "/train-ticket/static/js/0.ff3cc26c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/train-ticket/static/js/0.ff3cc26c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd5c3a6834f0903860d1",
+    "url": "/train-ticket/static/js/1.b5b28437.chunk.js"
+  },
+  {
+    "revision": "72e1321fbda9da5fe13478092f8d1222",
+    "url": "/train-ticket/static/js/1.b5b28437.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cef3fd16e8d70c00ad8e",
+    "url": "/train-ticket/static/js/10.8457a6a4.chunk.js"
+  },
+  {
+    "revision": "6f0ad54daca6af4bac12",
+    "url": "/train-ticket/static/js/index.94d15d3a.chunk.js"
+  },
+  {
+    "revision": "75104e727b314eb678a2",
+    "url": "/train-ticket/static/js/order.81fbd2f0.chunk.js"
+  },
+  {
+    "revision": "c045949d961d82d34773",
+    "url": "/train-ticket/static/js/query.848fdb7c.chunk.js"
+  },
+  {
+    "revision": "fcac88cc220881ce3b71",
+    "url": "/train-ticket/static/js/runtime-index.8bda5056.js"
+  },
+  {
+    "revision": "490ac7f0a40eb1f1524d",
+    "url": "/train-ticket/static/js/runtime-order.89afab12.js"
+  },
+  {
+    "revision": "6f7e19392fb5a7cab969",
+    "url": "/train-ticket/static/js/runtime-query.562e59dd.js"
+  },
+  {
+    "revision": "6667da45436e725b5884",
+    "url": "/train-ticket/static/js/runtime-ticket.86729fbb.js"
+  },
+  {
+    "revision": "9e67924f8bde494dc79d",
+    "url": "/train-ticket/static/js/ticket.d294006a.chunk.js"
+  },
+  {
+    "revision": "b672b5fbfa02c97e5511f5b00155bb8c",
+    "url": "/train-ticket/static/media/switch.b672b5fb.svg"
+  },
+  {
+    "revision": "7f389ddad7fa6fb3d8447bf7192fb58c",
+    "url": "/train-ticket/ticket.html"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/train-ticket/precache-manifest.295cd1e461d71a1ffe650cf3c2531d1f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/train-ticket/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
